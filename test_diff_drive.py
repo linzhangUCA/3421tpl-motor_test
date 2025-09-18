38d85649865539a750ab8f516e56ca8dcb6293f7
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2025-09-18 15:53:19 -0500

    Update test_diff_drive.py

--- test_diff_drive.py
+++ test_diff_drive.py
@@ -1,3 +1,6 @@
 from time import sleep
 
-# diff_driver = DiffDriver(left_ids=(), right_ids=(), stby_id=)
+diff_driver = DiffDriver(left_ids=(7, 9, 8), right_ids=(15, 13, 14), stby_id=12)
+### START CODING HERE
+
+### END CODING HERE
