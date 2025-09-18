ec0b15922264287a99719bce063827c1cce2cbd7
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2025-09-18 09:37:10 -0500

    Update octa_diff_driver.py
    add test code

--- octa_diff_driver.py
+++ octa_diff_driver.py
@@ -2,3 +2,32 @@
 Import correct modules and get started customizing them.
 https://github.com/linzhangUCA/3421example-motor_control
 """
+
+# TEST
+if __name__=="__main__":
+  # IMPORT
+  from time import sleep
+  
+  # SETUP/INSTANTIATE
+  dd = DiffDriver(right_ids=(9, 11, 10), left_ids=(15, 13, 14), stby_id=12)
+
+  # LOOP
+  dd.left_forward()
+  print("LF")
+  sleep(4)
+  dd.right_forward()
+  print("RF")
+  sleep(4)
+  dd.left_backward()
+  print("LB")
+  sleep(4)
+  dd.right_backward()
+  print("RB")
+  sleep(4)
+
+  # Terminate
+  dd.stop()
+  print("motors stopped.")
+  sleep(0.1)  # full stop
+  dd.disable()  # disable motor driver
+  print("motor driver disabled.")
